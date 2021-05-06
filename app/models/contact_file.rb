693870df6b90619d671214e28dfb89151769ca07
Eduardo PL <mini0n.000@gmail.com>
2021-05-06 17:15:07 -0500

    Change from API to Normal App

--- app/models/contact_file.rb
+++ app/models/contact_file.rb
@@ -1,3 +1,26 @@
 class ContactFile < ApplicationRecord
   belongs_to :user
+
+  COLUMNS = {
+    name: -1,       # Needed from params
+    birth: -1,      # Needed from params
+    phone: -1,      # Needed from params
+    email: -1,      # Needed from params
+    card: -1,       # Nedded from params
+    card_nums: -1,  # Calculated
+    franchise: -1   # Calculated
+  }.freeze
+
+  # Expeted: Hash of column names to index of the column:
+  # {
+  #   "name": 0,
+  #   "birth": 3,
+  #   "card": 2,
+  #   "email": 1
+  #   "phone": 4
+  # }
+  #
+  def match_file_columns
+    # TODO
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+
   resources :contact_errors
   resources :contact_files
   resources :contacts
